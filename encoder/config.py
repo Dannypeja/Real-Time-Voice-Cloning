6c20ebb56fc90cbbe956db85aba2f221120eceb3
Danilo Pejakovic <dape@MacBook-Pro-von-Danilo-2.local>
2021-12-08 09:02:48 +0100

    added mailabs preprocessing. adapted encoder preprocessing for commonVoice

--- encoder/config.py
+++ encoder/config.py
@@ -1,40 +1,26 @@
 librispeech_datasets = {
     "train": {
         "clean": ["LibriSpeech/train-clean-100", "LibriSpeech/train-clean-360"],
-        "other": ["LibriSpeech/train-other-500"]
+        "other": ["LibriSpeech/train-other-500"],
     },
-    "test": {
-        "clean": ["LibriSpeech/test-clean"],
-        "other": ["LibriSpeech/test-other"]
-    },
-    "dev": {
-        "clean": ["LibriSpeech/dev-clean"],
-        "other": ["LibriSpeech/dev-other"]
-    },
+    "test": {"clean": ["LibriSpeech/test-clean"], "other": ["LibriSpeech/test-other"]},
+    "dev": {"clean": ["LibriSpeech/dev-clean"], "other": ["LibriSpeech/dev-other"]},
 }
+mailabs_datasets = {"train": {"other": ["de_DE/by_book"]}}
+
+commonVoice_datasets = {"train": {"other": ["commonVoice"]}}
+
 libritts_datasets = {
     "train": {
         "clean": ["LibriTTS/train-clean-100", "LibriTTS/train-clean-360"],
-        "other": ["LibriTTS/train-other-500"]
+        "other": ["LibriTTS/train-other-500"],
     },
-    "test": {
-        "clean": ["LibriTTS/test-clean"],
-        "other": ["LibriTTS/test-other"]
-    },
-    "dev": {
-        "clean": ["LibriTTS/dev-clean"],
-        "other": ["LibriTTS/dev-other"]
-    },
+    "test": {"clean": ["LibriTTS/test-clean"], "other": ["LibriTTS/test-other"]},
+    "dev": {"clean": ["LibriTTS/dev-clean"], "other": ["LibriTTS/dev-other"]},
 }
 voxceleb_datasets = {
-    "voxceleb1" : {
-        "train": ["VoxCeleb1/wav"],
-        "test": ["VoxCeleb1/test_wav"]
-    },
-    "voxceleb2" : {
-        "train": ["VoxCeleb2/dev/aac"],
-        "test": ["VoxCeleb2/test_wav"]
-    }
+    "voxceleb1": {"train": ["VoxCeleb1/wav"], "test": ["VoxCeleb1/test_wav"]},
+    "voxceleb2": {"train": ["VoxCeleb2/dev/aac"], "test": ["VoxCeleb2/test_wav"]},
 }
 
 other_datasets = [
